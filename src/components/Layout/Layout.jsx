a5d4a3c61e178163beb4603e66bea8b2431b6ac8
Abeer Maged <162358745+Aalgehshry@users.noreply.github.com>
2026-06-16 23:09:15 +0300

    Fix Navbar import and update Layout structure
    Refactor Layout component structure and fix Navbar import.

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import style from "./Layout.module.css";
-
+import Navbar from "./../Navbar/Navbar";
 import Footer from "./../Footer/Footer";
 import { Outlet } from "react-router-dom";
-import Navabar from "./../Navbar/Navbar";
 export default function Layout() {
   return (
-    <>
-      <Navabar />
-      <div className=" container mx-auto w-[80%] py-20 ">
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="container mx-auto w-[80%] py-20 flex-grow">
         <Outlet />
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
